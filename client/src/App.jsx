9876e4056c05c16483d5f5c551077884b264cb1b
Geleta <geletasisay44@gmail.com>
2025-11-30 05:06:12 -0800

    added the headcer component and updated the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"; 
+import About from "./pages/About";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUP";
+import Profile from "./pages/Profile";
+import Header from "./component/Header";
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>hello</h1>
-      
-    </div>
-  )
-}
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Signin" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
